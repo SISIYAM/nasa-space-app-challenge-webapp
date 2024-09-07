57be2495834e2ba1b8b8f797b49bb556c5d6e71e
MD Saymum Islam Siyam <si31siyam@gmail.com>
2024-09-07 16:10:23 +0600

    initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import { title, subtitle } from "@/components/primitives";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className={title({ color: "violet" })}>About</h1>
+    </div>
+  );
+}
